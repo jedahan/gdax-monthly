20e2348cc80f615e18118d6213800dd4555d07b2
Jonathan Dahan <hi@jonathan.is>
2017-12-18 12:29:05 -0500

    new

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 global.fetch = require('node-fetch')
 const cc = require('cryptocompare')
 
-function getDaysInMonth(month, year) {
-  let date = new Date(year, month, 1)
-  var days = []
-  while (date.getMonth() === month) {
-    days.push(new Date(date))
-    date.setDate(date.getDate() + 1)
-  }
-  return days
+function getAveragePriceForMonth(year, month) {
+  return new Promise((resolve, reject) => {
+    cc
+      .histoDay('ETH', 'USD', { timestamp: new Date(`${year}-${(month+1)}-1`) })
+      .then(prices => {
+          resolve(prices
+            .map(price => price.close)
+            .reduce((sum, price) => { return sum + price }, 0)
+            / prices.length)
+        })
+      })
 }
 
-const november = getDaysInMonth(11, 17)
+getAveragePriceForMonth(2017, 10).then(average => console.log(`October: ${average}`))
+getAveragePriceForMonth(2017, 11).then(average => console.log(`November: ${average}`))
 
-Promise.all(
-  november.map(day => cc.priceHistorical('ETH', ['USD'], day))
-).then(prices => {
-  const sum = prices
-    .map(price => price['USD'])
-    .reduce((sum, value) => {sum + value}, 0)
-  console.log("average price for nov")
-  console.log(sum / november.length)
-})
+const Gdax = require('gdax')
+const gdax = new Gdax.PublicClient('ETH-USD')
+
+function getAveragePriceForMonthCoinbase(year, month) {
+  return new Promise((resolve, reject) => {
+    gdax
+      .getProductHistoricRates({granularity: 60 * 60 * 24, start: `${year}-${month}-1`, end: `${year}-${(month+1)}-1`},
+        (err, response) => {
+          const days = JSON.parse(response.body).length
+          const prices = JSON.parse(response.body)[0]
+          resolve(prices
+            .map(price => prices[3])
+            .reduce((sum, price) => { return sum + price }, 0)
+            / prices.length
+          )
+        }
+      )
+  })
+}
+
+getAveragePriceForMonthCoinbase(2017, 11).then(average => console.log(`GDAX ETH-USD November: ${average}`))
